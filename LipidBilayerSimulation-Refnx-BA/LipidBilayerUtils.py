624a04c83208335b15560263c3bf8750e1aa3f07
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2019-03-12 11:51:56 +0100

    Read three columns from data files and do not normalize intensities; add __getitem__ method to SampleParameters class

--- LipidBilayerSimulation-Refnx-BA/LipidBilayerUtils.py
+++ LipidBilayerSimulation-Refnx-BA/LipidBilayerUtils.py
@@ -78,7 +78,6 @@ class SampleParameters():
     def __init__(self,params={}):
         assert isinstance(params, dict), "SampleParameters class must be initialized by an instance of dict"
 
-
         self.solvent_sld = params["solvent_sld"] if "solvent_sld" in params else self._si_sld
         self.d2o_sld = params["d2o_sld"] if "d2o_sld" in params else self._d2o_sld
         self.h2o_sld = params["h2o_sld"] if "h2o_sld" in params else self._h2o_sld
@@ -107,10 +106,12 @@ class SampleParameters():
         self.head_a_sfv  = params["head_a_sfv"]  if "head_a_sfv"  in params else self._head_a_sfv
         self.sio2_sfv    = params["sio2_sfv"]    if "sio2_sfv"    in params else self._sio2_sfv
 
-
         self.d2o_bkg    = params["d2o_bkg"]    if "d2o_bkg"    in params else self._d2o_bkg
         self.h2o_bkg    = params["h2o_bkg"]    if "h2o_bkg"    in params else self._h2o_bkg
         self.hdmix_bkg    = params["hdmix_bkg"]    if "hdmix_bkg"    in params else self._hdmix_bkg
+
+    def __getitem__(self, key):
+        return eval("self."+key)
 
 
 def overall_sld(original_sld,solvent_sld,solvent_fraction):
@@ -181,8 +182,8 @@ def get_real_data(filename):
     - Get a numpy array from the data file.
     - Normalize intensity values.
     """
-    real_data = np.loadtxt(filename, usecols=(0, 1))
-    real_data[:, 1] /= real_data[0, 1]
+    real_data = np.loadtxt(filename, usecols=(0, 1, 2))
+    #real_data[:, 1] /= real_data[0, 1]
     return real_data
 
 
